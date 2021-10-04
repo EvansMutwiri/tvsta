6402bbf283b9843a5ed940d20d13334eadeea03c
EvansMutwiri <evans.mwiti@student.moringaschool.com>
2021-10-04 21:50:35 +0300

    bind layout instantiate adapter create adapter

--- app/src/main/java/com/evans/tvsta/MainActivity.kt
+++ app/src/main/java/com/evans/tvsta/MainActivity.kt
@@ -2,10 +2,22 @@ package com.evans.tvsta
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import com.evans.tvsta.databinding.ActivityMainBinding
+import com.evans.tvsta.viewmodel.TvShowViewModel
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val viewModel: TvShowViewModel by viewModels()
+    private lateinit var tvShowAdapter: TvShowAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
---
+++ app/src/main/java/com/evans/tvsta/adapter/TvShowAdapter.kt
@@ -0,0 +1,9 @@
+package com.evans.tvsta.adapter
+
+import androidx.recyclerview.widget.RecyclerView
+
+//add adapter after instantiating in mainactivity before viewholder
+
+class TvShowAdapter: RecyclerView.Adapter<MyViewHolder>() {
+    inner class MyViewHolder(val binding: )
+}
